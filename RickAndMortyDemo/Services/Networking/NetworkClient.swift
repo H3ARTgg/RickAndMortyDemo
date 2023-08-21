7febeed6d03a6133274eb1407280c291421ca2bf
H3ARTgg <nomaxismax@gmail.com>
2023-08-22 02:05:33 +0300

    feat: network client changes

--- RickAndMortyDemo/Services/Networking/NetworkClient.swift
+++ RickAndMortyDemo/Services/Networking/NetworkClient.swift
@@ -22,6 +22,10 @@ protocol NetworkClient {
         request: NetworkRequest,
         type: T.Type
     ) -> AnyPublisher<T, NetworkError>
+    
+    func networkPublisher(
+        request: NetworkRequest
+    ) -> AnyPublisher<Data, NetworkError>
 }
 
 struct DefaultNetworkClient: NetworkClient {
@@ -115,10 +119,26 @@ struct DefaultNetworkClient: NetworkClient {
         guard let urlRequest = create(request: request) else {
             return Fail(error: NetworkError.badRequest).eraseToAnyPublisher()
         }
-        
         return session.dataTaskPublisher(for: urlRequest)
             .map(\.data)
             .decode(type: type.self, decoder: decoder)
+            .mapError { (error) -> NetworkError in
+                switch error {
+                case is URLError:
+                    return NetworkError.addressUnreachable(request.endpoint)
+                default:
+                    return NetworkError.invalidResponse
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func networkPublisher(request: NetworkRequest) -> AnyPublisher<Data, NetworkError> {
+        guard let urlRequest = create(request: request) else {
+            return Fail(error: NetworkError.badRequest).eraseToAnyPublisher()
+        }
+        return session.dataTaskPublisher(for: urlRequest)
+            .map(\.data)
             .mapError { (error) -> NetworkError in
                 switch error {
                 case is URLError:
