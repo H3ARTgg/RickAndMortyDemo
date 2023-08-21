194e0a510848a1621b025125857feef71902dd8e
H3ARTgg <nomaxismax@gmail.com>
2023-08-21 18:03:37 +0300

    refactor: modules factory

--- RickAndMortyDemo/AppBasic/ModulesFactory.swift
+++ RickAndMortyDemo/AppBasic/ModulesFactory.swift
@@ -1,15 +1,15 @@
 protocol ModulesFactoryProtocol: AnyObject {
+    var networkManager: NetworkManagerProtocol { get }
     func makeCharactersListView() -> (view: Presentable, coordination: CharactersListCoordination)
     //func makeCharacterInfoView() -> (view: Presentable, coordination: CharacterInfoCoordination)
 }
 
 final class ModulesFactory: ModulesFactoryProtocol {
+    let networkManager: NetworkManagerProtocol = NetworkManager(networkService: DefaultNetworkClient())
+    
     func makeCharactersListView() -> (view: Presentable, coordination: CharactersListCoordination) {
         let listVM = CharactersListViewModel()
         let list = CharactersListViewController(viewModel: listVM)
         return (list, listVM)
     }
-    
-    //func makeCharacterInfoView() -> (view: Presentable, coordination: CharacterInfoCoordination) {
-    //}
 }
