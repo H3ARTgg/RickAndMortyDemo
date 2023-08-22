8c05ae235053e37dd3ff88b64c5803cf8591fbac
H3ARTgg <nomaxismax@gmail.com>
2023-08-22 04:20:58 +0300

    chore: change access modifier for function

--- RickAndMortyDemo/Services/Networking/Requests/CharactersRequest.swift
+++ RickAndMortyDemo/Services/Networking/Requests/CharactersRequest.swift
@@ -7,7 +7,7 @@ struct CharactersRequest: NetworkRequest {
         self.endpoint = URL(string: "https://rickandmortyapi.com/api/character/" + makeStringOfRange(characterIdsRange))
     }
     
-    func makeStringOfRange(_ range: Range<Int>) -> String {
+    private func makeStringOfRange(_ range: Range<Int>) -> String {
         range.sorted().map { String($0) }.joined(separator: ",")
     }
 }
