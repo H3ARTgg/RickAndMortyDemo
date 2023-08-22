7fbfd7d657bd54f77b0e693965353d018b66c23e
H3ARTgg <nomaxismax@gmail.com>
2023-08-22 06:14:15 +0300

    feat: add new font

--- RickAndMortyDemo/Helpers/UI/UIFont + Extension.swift
+++ RickAndMortyDemo/Helpers/UI/UIFont + Extension.swift
@@ -4,4 +4,5 @@ extension UIFont {
     static let title28 = UIFont.systemFont(ofSize: 28, weight: .bold)
     static let title22 = UIFont.systemFont(ofSize: 22, weight: .bold)
     static let title17 = UIFont.systemFont(ofSize: 17, weight: .bold)
+    static let regular16 = UIFont.systemFont(ofSize: 16, weight: .regular)
 }
