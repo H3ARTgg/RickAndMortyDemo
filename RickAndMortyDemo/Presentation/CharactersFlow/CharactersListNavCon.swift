ec7e9120bbb85784fc0cd97867552dec4eb0cfc6
H3ARTgg <nomaxismax@gmail.com>
2023-08-22 18:02:33 +0300

    fix: light appearance

--- RickAndMortyDemo/Presentation/CharactersFlow/CharactersListNavCon.swift
+++ RickAndMortyDemo/Presentation/CharactersFlow/CharactersListNavCon.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-final class CharactersListNavCon: UINavigationController {}
+final class CharactersListNavCon: UINavigationController {
+    init() {
+        super.init(nibName: nil, bundle: .main)
+        self.navigationBar.barStyle = .black
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
