59a845c49f94fa823761fb11e4cfd2547f21aa7d
H3ARTgg <nomaxismax@gmail.com>
2023-08-22 08:15:36 +0300

    feat: add new localization

--- RickAndMortyDemo/Helpers/String + Extension.swift
+++ RickAndMortyDemo/Helpers/String + Extension.swift
@@ -2,8 +2,14 @@ import Foundation
 
 enum LocalizedKeys: String {
     case characters = "characters"
+    case info = "info"
+    case origin = "origin"
+    case episodes = "episodes"
 }
 
 extension String {
     static let characters = NSLocalizedString(LocalizedKeys.characters.rawValue, comment: "")
+    static let info = NSLocalizedString(LocalizedKeys.info.rawValue, comment: "")
+    static let origin = NSLocalizedString(LocalizedKeys.origin.rawValue, comment: "")
+    static let episodes = NSLocalizedString(LocalizedKeys.episodes.rawValue, comment: "")
 }
